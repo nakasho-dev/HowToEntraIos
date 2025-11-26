f2ee10f804302fd8b140c47a7496862b94737e8b
nakasho <19910245+nakasho-dev@users.noreply.github.com>
2025-11-26 22:59:58 +0900

    Initial Commit

---
+++ HowToEntraIos/HowToEntraIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToEntraIosApp.swift
+//  HowToEntraIos
+//
+//  Created by Shinya Nakajima on 2025/11/26.
+//
+
+import SwiftUI
+
+@main
+struct HowToEntraIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HowToEntraIosTests/HowToEntraIosTests.swift
@@ -0,0 +1,17 @@
+//
+//  HowToEntraIosTests.swift
+//  HowToEntraIosTests
+//
+//  Created by Shinya Nakajima on 2025/11/26.
+//
+
+import Testing
+@testable import HowToEntraIos
+
+struct HowToEntraIosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
